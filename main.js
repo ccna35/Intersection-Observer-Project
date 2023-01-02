0ea5fe9778a95e884fa6c8696238776484b491a6
ccna35 <shawkykhalil@gmail.com>
2023-01-02 22:36:11 +0200

    "Added more elements"

--- main.js
+++ main.js
@@ -5,22 +5,25 @@ const observer = new IntersectionObserver(
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add("show");
-        console.log(entry);
-      } else {
-        entry.target.classList.remove("show");
       }
     });
-    // entries.forEach((entry) => entry.target.classList.add("show"));
-    //   console.log(entries);
   },
   { threshold: 0.5 }
 );
-
-// observer.observe(cards[2]);
 
 const callback = (e) => {
   console.log(e.target);
 };
 
 cards.forEach((card) => observer.observe(card));
-// cards.forEach((card) => card.addEventListener("click", callback));
+
+const dropdownBtns = document.querySelectorAll(".dropdown");
+
+dropdownBtns.forEach((btn) => {
+  btn.addEventListener("mouseover", () => {
+    btn.querySelector("ul").classList.add("show");
+  });
+  btn.addEventListener("mouseleave", () => {
+    btn.querySelector("ul").classList.remove("show");
+  });
+});
